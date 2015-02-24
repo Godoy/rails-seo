ff243757412770944f4bb901d14fbb1a58d14501
Adriano Godoy <godoy.ccp@gmail.com>
2015-02-23 22:09:20 -0300

    Listagem e exibicao basica dos conteudos. Links entre as paginas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'events/index'
-  get 'events/show'
-  get 'news/index'
-  get 'news/show'
+
+  get 'events' => 'events#index', as: :events
+  get 'events/:id' => 'events#show', as: :event
+
+  get 'news' => 'news#index', as: :news_index
+  get 'news/:id' => 'news#show', as: :news
+  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
